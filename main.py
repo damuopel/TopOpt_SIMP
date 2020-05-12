417f74f5946629470dbf933d1c27d1ebfa917c96
David <davidmunozpellicer@gmail.com>
2020-05-12 16:02:35 +0200

    Fix several (plenty) issues and added the plot functions and the user inputs.

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from scipy.sparse import csc_matrix,linalg
 
 # Constants
 defaultInputs = 6
+tol = 1e-6
 h = 1.0 # Elements size
-E = 2.1e11# Young's Module
+E = 1000 # Young's Module
 nu = 0.3 # Poisson ratio
 t = 1.0 # Thickness
 nodesElement = 4
@@ -51,6 +52,7 @@ class TopOpt():
         self.p = p
         self.c = 0.0
         self.dc = np.zeros(x.shape)
+        self.dv = np.ones(x.shape)
          
     def CreateFilter(self,Mesh):
         self.H = np.zeros((Mesh.nElms,Mesh.nElms))
@@ -69,16 +71,39 @@ class TopOpt():
     
     def OptimalityCriteria(self,Mesh):
         # Bisection Method
-        l1 = 0.0
-        l2 = 1e6
+        l1 = np.amin(np.absolute(self.dc))/np.amax(np.absolute(self.dv))
+        l2 = np.amax(np.absolute(self.dc))/np.amin(np.absolute(self.dv))
         move = 0.2
-        while (abs(l2-l1) > 1e-4):
+        while abs((l2-l1)/(l2+l1))>=tol:
             lmid = 0.5*(l1+l2)
-            self.x = np.maximum(1e-3,np.maximum(self.x-move,np.minimum(1.0,np.minimum(self.x+move,self.x*np.sqrt(-self.dc/lmid)))))
-            if sum(self.x) - self.v*Mesh.nElms > 0:
+            xnew = np.maximum(1e-3,np.maximum(self.x-move,np.minimum(1.0,np.minimum(self.x+move,self.x*np.sqrt(-self.dc/(self.dv*lmid))))))
+            if np.sum(xnew) - self.v*Mesh.nElms > 0:
                 l1 = lmid
             else:
                 l2 = lmid
+        self.x = xnew
+        
+class Plots():
+    def Displacements(self,Mesh,Solution):
+        # Plot Displacements
+        x = np.arange(0,Mesh.dofs,2)
+        y = np.arange(1,Mesh.dofs,2)
+        ux = Solution[x]
+        uy = Solution[y]
+        d = (ux**2+uy**2)**0.5
+        x = Mesh.xy[0,:].reshape(ny+1,nx+1)
+        y = Mesh.xy[1,:].reshape(ny+1,nx+1)
+        plt.pcolor(x,y,d.reshape(ny+1,nx+1))
+        plt.colorbar()
+        plt.show()
+    
+    def MaterialDistribution(self,Top,):
+        # Plot material distribution
+        plt.clf()
+        plt.pcolor(top.x.reshape(ny,nx),edgecolors='k',linewidth=1)
+        plt.colorbar()
+        plt.draw()
+        plt.pause(1e-6)
                      
 class FEM():        
     def K(self,Mesh,Material,Top):
@@ -103,9 +128,10 @@ class FEM():
         elsDofs = 2*np.kron(Mesh.Topology,np.ones((2,1)))+np.tile(np.array([[0],[1]]),[4,Mesh.nElms])
         row = np.kron(elsDofs,np.ones((1,dofsElements))).T.flatten()
         col = np.kron(elsDofs,np.ones((dofsElements,1))).T.flatten()
-        data = np.repeat(Top.x,dofsElements**2,axis=1)**3*np.tile(Ke.flatten(),(Mesh.nElms,1))
+        K0 = np.tile(Ke.flatten(),(Mesh.nElms,1))
+        data = np.repeat(Top.x,dofsElements**2,axis=1)**Top.p*K0
         K = csc_matrix((data.flatten(),(row,col)),shape=(Mesh.dofs,Mesh.dofs))
-        return K,data
+        return K,K0
     
     def Loads(self,Mesh):
         self.F = np.zeros((Mesh.dofs,1))
@@ -119,7 +145,7 @@ class FEM():
             forceNodes = nodes[np.where(np.logical_and(xMax,yMin))]
             forceDofs = np.array([2*forceNodes,2*forceNodes+1])
             fx = 0
-            fy = -1000
+            fy = -1
             self.F[forceDofs[0,:]] = fx
             self.F[forceDofs[1,:]] = fy
             
@@ -161,28 +187,31 @@ def ShapeFunctions(Xi,Eta,dFlag):
         N = np.array([N1,N2,N3,N4])       
     return N
 
+
 if __name__ == '__main__':
     # User input
     if len(sys.argv) == defaultInputs:
-        nx = sys.argv[1]
-        ny = sys.argv[2]
-        r = sys.argv[3]
-        v = sys.argv[4]
-        p = sys.argv[5]
+        nx = int(sys.argv[1])
+        ny = int(sys.argv[2])
+        r = float(sys.argv[3])
+        v = float(sys.argv[4])
+        p = float(sys.argv[5])
     else:
         nx = 30
         ny = 20
         r = 1.5
         v = 0.3
         p = 3
-    # Initialize variables
+    # Initialize FEM variables
     material = Material()
     mesh = Mesh(nx,ny)
     fem = FEM()
-    fig = plt.figure()
-    ax = fig.gca()
+    # Initialize plot options
+    plot = Plots()
+    plt.ion()
+    ax = plt.gca()
     ax.axis('equal')
-    # Initialize Optimization
+    # Initialize Optimization variables
     x = v*np.ones((nx*ny,1))
     xold = x
     top = TopOpt(x,xold,v,r,p)
@@ -191,15 +220,14 @@ if __name__ == '__main__':
     it = 0
     while change > 1e-4 and it < 100:
         it += 1
-        # Plot material distribution
-        ax.clear()
-        plt.pcolor(top.x.reshape(ny,nx),edgecolors='k',linewidth=1)
-        plt.show()
+        # Plot Material distribution
+        plot.MaterialDistribution(top)
         # Solve Elasticty Problem
         K,K0 = fem.K(mesh,material,top)
         fem.Loads(mesh)
         u = fem.Solver(mesh,K)
         # Evaluate Compliance and Sensitivities
+        top.c = 0
         for iElm in range(0,mesh.nElms):
             iNodes = mesh.Topology[:,iElm]
             iDofs = np.array([2*iNodes,2*iNodes+1]).T.flatten()
@@ -210,8 +238,12 @@ if __name__ == '__main__':
         top.FilterSensitivities()
         top.OptimalityCriteria(mesh)
         change = np.amax(abs(top.x-top.xold))
-        print(top.c,change)
+        print('Iteration: {} | Compliance: {} | Change: {}'.format(it,top.c[0,0],change))
         top.xold = top.x
+        
+        
+        
+        
         
         
         
